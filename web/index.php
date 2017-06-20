0b5a32be6355995beafab705e86446a7aa2b2549
Felipe Martins <me@fefas.net>
2017-06-19 21:52:30 -0300

    Refactor index to use Slim framework

--- web/index.php
+++ web/index.php
@@ -1,1 +1,15 @@
 <?php
+
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Message\ResponseInterface as Response;
+use Slim\App;
+
+require __DIR__.'/../vendor/autoload.php';
+
+$app = new Slim\App();
+
+$app->post('/check-username', function (Request $request, Response $response) {
+    return $response->withStatus(200);
+});
+
+$app->run();
